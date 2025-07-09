9fb7475981ccd6d4d08cab947e4631e1c19dd521
poragjyotigogoi1991 <156182359+poragjyotigogoi1991@users.noreply.github.com>
2025-07-09 14:50:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -286,7 +286,7 @@ function LoadControls() {
     button.style.pointerEvents = "auto";
   }
 
-  styleControls(worldMapButton);
+ // styleControls(worldMapButton);
   styleControls(plusButton);
   styleControls(minusButton);
 
