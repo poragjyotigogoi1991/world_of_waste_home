3a24c10bebd5e1d5b247e2e082f7e382aed9b773
Chinmoy Raj Deka <65728129+ChinmoyRaj123@users.noreply.github.com>
2025-08-27 16:16:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ function getTotalWasteCountryWise() {
       );
     }
   });
-  console.log("allCountries ss",allCountries):
+  console.log("allCountries ss",allCountries);
   console.log("Country wise waste :::", countryWiseTotalWaste);
   highlightAllStates(allCountries);
 }
